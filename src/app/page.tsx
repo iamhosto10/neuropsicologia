226f98b6f4b75720143ca9ef61e59f921207e8ec
Gerardo Ramirez <gerardoramirez4400@gmail.com>
2026-01-02 15:12:04 -0500

    fix grid in mobile and fix margin and padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturesGrid from "@/components/features-grid/features-grid";
 import { HeroSection } from "@/components/heroSection/hero-section";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[#F9FAFB] ">
       <HeroSection />
+      <FeaturesGrid />
     </main>
   );
 }
--- src/components/features-grid/features-grid.tsx
+++ src/components/features-grid/features-grid.tsx
@@ -1,36 +1,36 @@
-import { Puzzle, GraduationCap, ListTodo, Gamepad2 } from 'lucide-react';
-import React from 'react';
+import { Puzzle, GraduationCap, ListTodo, Gamepad2 } from "lucide-react";
+import React from "react";
 
 const features = [
   {
     icon: <Puzzle className="h-8 w-8 text-[#22C55E]" />,
-    title: 'Actividades',
-    subtitle: 'Explora y diviértete',
+    title: "Actividades",
+    subtitle: "Explora y diviértete",
   },
   {
     icon: <GraduationCap className="h-8 w-8 text-[#22C55E]" />,
-    title: 'Cursos',
-    subtitle: 'Aprende a tu ritmo',
+    title: "Cursos",
+    subtitle: "Aprende a tu ritmo",
   },
   {
     icon: <ListTodo className="h-8 w-8 text-[#22C55E]" />,
-    title: 'Checklists',
-    subtitle: 'Organiza tus tareas',
+    title: "Checklists",
+    subtitle: "Organiza tus tareas",
   },
   {
     icon: <Gamepad2 className="h-8 w-8 text-[#22C55E]" />,
-    title: 'Juegos',
-    subtitle: 'Desafía tu mente',
+    title: "Juegos",
+    subtitle: "Desafía tu mente",
   },
 ];
 
 const FeaturesGrid: React.FC = () => {
   return (
-    <div className="grid grid-cols-2 gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-4 lg:gap-8">
+    <div className="grid grid-cols-2 gap-4 md:gap-6 lg:grid-cols-4 lg:gap-8 mx-2 md:mx-6 lg:mx-10 py-5">
       {features.map((feature) => (
         <div
           key={feature.title}
-          className="flex flex-col items-center text-center p-6 bg-white rounded-2xl border border-green-100 hover:scale-105 transition-transform"
+          className="flex flex-col items-center text-center p-6 rounded-2xl border border-green-100 bg-white hover:scale-110 hover:cursor-pointer transition-transform"
         >
           {feature.icon}
           <h3 className="mt-4 font-bold">{feature.title}</h3>

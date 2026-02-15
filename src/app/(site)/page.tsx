125912655bd4063ce3239be33017e97b4c05cb61
Gerardo Ramirez <gerardoramirez4400@gmail.com>
2026-02-15 13:45:32 -0500

    fix when profile when log in

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,8 +2,15 @@ import FeaturesGrid from "@/components/features-grid/features-grid";
 import { HeroSection } from "@/components/heroSection/hero-section";
 import PopularActivities from "@/components/popular-activities/popular-activities";
 import { ToolsSection } from "@/components/tools-section/tools-section";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/dashboard");
+  }
   return (
     <main className="bg-[#F9FAFB] ">
       <HeroSection />

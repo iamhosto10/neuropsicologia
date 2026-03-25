04e9bb54287c7c9d4c44b9a71b35bd15adb317a2
Gerardo Ramirez <gerardoramirez4400@gmail.com>
2026-03-25 14:59:07 -0500

    add test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,19 +1,255 @@
-import Image from "next/image";
+import InlineGameWrapper from "@/components/lesson-detail/inline-game-wrapper";
 
 const Page = () => {
   return (
-    <div className="flex gap-1 max-w-[100vw] px-11 pt-17 bg-background">
+    <div className="flex flex-col gap-1 max-w-4xl px-11 pt-17 mx-auto bg-background">
       {/* Contenedor relativo para controlar el tamaño */}
-      <div className="mx-auto relative h-7 w-24 border-b border-border">
-        {/* <Image
-          src="https://cdn.builder.io/api/v1/image/assets%2Fd96fe6595243498081d815fe03062cfa%2F0f04b37284dc4023bf0b9354b91b1b54?format=webp"
-          alt="Descripción de la imagen"
-          fill // Llena el contenedor padre
-          className="object-contain" // Ajusta sin deformar
-        /> */}
-      </div>
-      <div>hola</div>
-      <div>hola</div>
+      <h1 className="text-4xl font-bold text-primary mb-4">
+        Página para testear los juegos en distintas dificultades
+      </h1>
+      <h2 className="text-2xl font-semibold text-blue-500 border-b pb-1">
+        Limpieza Espacial (Atención Selectiva)
+      </h2>
+      <h3 className="text-lg font-medium text-green-500">Facil</h3>
+      <InlineGameWrapper
+        value={{
+          gameType: "cleanup",
+          difficulty: "easy",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Medio</h3>
+      <InlineGameWrapper
+        value={{
+          gameType: "cleanup",
+          difficulty: "medium",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Dificil</h3>
+      <InlineGameWrapper
+        value={{
+          gameType: "cleanup",
+          difficulty: "hard",
+        }}
+      />
+      <h2 className="text-2xl font-semibold text-blue-500 border-b pb-1">
+        {" "}
+        Centinela Satélite (Atención Sostenida)
+      </h2>
+      <h3 className="text-lg font-medium text-green-500">Facil</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "satellite",
+          difficulty: "easy",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Medio</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "satellite",
+          difficulty: "medium",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Dificil</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "satellite",
+          difficulty: "hard",
+        }}
+      />
+      <h2 className="text-2xl font-semibold text-blue-500 border-b pb-1">
+        {" "}
+        Campo de Asteroides (Control Inhibitorio)
+      </h2>
+      <h3 className="text-lg font-medium text-green-500">Facil</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "asteroid",
+          difficulty: "easy",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Medio</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "asteroid",
+          difficulty: "medium",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Dificil</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "asteroid",
+          difficulty: "hard",
+        }}
+      />
+      <h2 className="text-2xl font-semibold text-blue-500 border-b pb-1">
+        {" "}
+        Comunicador Inverso (Flexibilidad)
+      </h2>
+      <h3 className="text-lg font-medium text-green-500">Facil</h3>
+      <InlineGameWrapper
+        value={{
+          gameType: "reverse",
+          difficulty: "easy",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Medio</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "reverse",
+          difficulty: "medium",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Dificil</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "reverse",
+          difficulty: "hard",
+        }}
+      />
+
+      <h2 className="text-2xl font-semibold text-blue-500 border-b pb-1">
+        {" "}
+        Panel de Seguridad (Memoria Visoespacial)
+      </h2>
+      <h3 className="text-lg font-medium text-green-500">Facil</h3>
+      <InlineGameWrapper
+        value={{
+          gameType: "memori",
+          difficulty: "easy",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Medio</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "memori",
+          difficulty: "medium",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Dificil</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "memori",
+          difficulty: "hard",
+        }}
+      />
+      <h2 className="text-2xl font-semibold text-blue-500 border-b pb-1">
+        {" "}
+        Gran Evasión (Multitarea y Atención Dividida)
+      </h2>
+      <h3 className="text-lg font-medium text-green-500">Facil</h3>
+      <InlineGameWrapper
+        value={{
+          gameType: "multitask",
+          difficulty: "easy",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Medio</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "multitask",
+          difficulty: "medium",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Dificil</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "multitask",
+          difficulty: "hard",
+        }}
+      />
+      <h2 className="text-2xl font-semibold text-blue-500 border-b pb-1">
+        {" "}
+        Laboratorio de Carga (Memoria N-Back)
+      </h2>
+      <h3 className="text-lg font-medium text-green-500">Facil</h3>
+      <InlineGameWrapper
+        value={{
+          gameType: "cargo_n_back",
+          difficulty: "easy",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Medio</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "cargo_n_back",
+          difficulty: "medium",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Dificil</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "cargo_n_back",
+          difficulty: "hard",
+        }}
+      />
+      <h2 className="text-2xl font-semibold text-blue-500 border-b pb-1">
+        {" "}
+        Tormenta de Nebulosa (Control de Interferencia)
+      </h2>
+      <h3 className="text-lg font-medium text-green-500">Facil</h3>
+      <InlineGameWrapper
+        value={{
+          gameType: "nebula_storm",
+          difficulty: "easy",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Medio</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "nebula_storm",
+          difficulty: "medium",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Dificil</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "nebula_storm",
+          difficulty: "hard",
+        }}
+      />
+      <h2 className="text-2xl font-semibold text-blue-500 border-b pb-1">
+        {" "}
+        Decodificador de Señales (Velocidad Procesamiento)
+      </h2>
+      <h3 className="text-lg font-medium text-green-500">Facil</h3>
+      <InlineGameWrapper
+        value={{
+          gameType: "signal_decoder",
+          difficulty: "easy",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Medio</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "signal_decoder",
+          difficulty: "medium",
+        }}
+      />
+      <h3 className="text-lg font-medium text-green-500">Dificil</h3>
+
+      <InlineGameWrapper
+        value={{
+          gameType: "signal_decoder",
+          difficulty: "hard",
+        }}
+      />
     </div>
   );
 };
